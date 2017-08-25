b9b41e55407c339ab72e925d1cb1fbd7b6ae8cda
rhoneybul <robert.honeybul@rthdatasolutions.com>
2017-08-25 09:33:24 +0800

    updated wikipedia scrape to follow data schema, and write to csv file as well as json file

--- travel-ban/terrorism-incidents/wikipedia-scrape.py
+++ travel-ban/terrorism-incidents/wikipedia-scrape.py
@@ -6,9 +6,30 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import os
 import shutil
+import re
 import json
 import argparse
+from jsonschema import validate
 
+'''
+========================
+	DATA SCHEMA
+========================
+
+{
+	"Injured: 
+	"Dead": 
+	"Location":  
+	"Date":  
+	"Perpetrators": 
+	"Type": 
+	"Details": 
+}
+
+If any of the data does not fit the schema, an integer entry is given None, 
+and a string entry is given "Not Applicable"
+
+'''
 def start_driver():
 	return webdriver.Firefox()
 
@@ -16,7 +37,6 @@ def get_page_links(driver, ROOT_URL, years, list_links):
 	all_years = map(str, years)
 	driver.get(ROOT_URL)
 	list_links = driver.find_elements_by_css_selector(list_links)
-	print len(list_links)
 	page_links = []
 	for j, link in enumerate(list_links):
 		for year in all_years:
@@ -42,15 +62,15 @@ def read_page_links(filename) :
 			page_links.append((link, text))
 	return page_links
 
-def get_all_incidents(driver, page_links, outfile):
+def get_all_incidents(driver, page_links, outfile, data_keys):
 	all_incidents = {}
 	for i, (link, text) in enumerate(page_links):
 		print 'GETTING PAGE #{} OF {}'.format(i+1, len(page_links))
-		all_incidents[text] = get_incidents(driver, link)
+		all_incidents[text] = get_incidents(driver, link, data_keys)
 		write_all_incidents(all_incidents, outfile)
 	return all_incidents
 
-def get_incidents(driver, url):
+def get_incidents(driver, url, data_keys):
 	driver.get(url)
 	tables = driver.find_elements_by_css_selector('table.wikitable')
 	incidents = []
@@ -72,11 +92,15 @@ def get_incidents(driver, url):
 			for entry in row.find_elements_by_css_selector("td"):
 				tableRow.append(entry.text)
 			tableVals.append(tableRow)
-
 		for row in tableVals:
 			incident = {}
 			for val, header in zip(row, headerVals):
-				incident[header] = val
+				if header in data_keys:
+					incident[header] = val
+			## make the empty values equal to "Not Available"
+			for key in data_keys:
+				if key not in incident:
+					incident[key] = "Not Available"
 			incidents.append(incident)
 	return incidents
 
@@ -95,11 +119,25 @@ def parse_arguments():
 	parser.add_argument('-o', '--outfile', help='Specify the outpath for the data to be written to', type=str, required=True)
 	parser.add_argument('-y', '--years', nargs='+', help='Specify the years for which to get the data (multiple arguments accepted)', type=int, required=True)
 	parser.add_argument('-l', '--list-links', help='Specify the css selector for which the page links are gathered\n Default is "div.column-count-3 li a"', type=str, required=False)
+	parser.add_argument('-k', '--data-keys', help='Specify the data keys to gather, from the list [Injured, Dead, Location, Date, Perpetrators, Type, Details]', type=str, required=False, nargs='+')
 	return parser.parse_args()
 
+def write_incidents_to_csv(incidents, outfile, data_keys):
+	headers = ['Dataset'] + sorted(data_keys)
+	with open(outfile, 'w') as f:
+		f.write(', '.join(headers))
+		for dataset, data_values in incidents.iteritems():
+			for data in data_values:
+				assert(sorted(data.keys()) == sorted(data_keys))
+				row_values = [re.sub('\n', '', dataset)]
+				for header in headers[1:]:
+					row_values.append(re.sub('\n', '', data[header].encode('ascii', 'ignore')))
+				f.write(', '.join(row_values)+'\n')
+			
 def main():
 	args = parse_arguments()
-	links_outfile = args.outfile.split(".json")[0]+'.txt' 
+	links_outfile = args.outfile.split(".json")[0]+'.txt'
+	csv_outfile = args.outfile.split(".json")[0]+'.csv' 
 	driver = start_driver()
 	if args.root_url is None:
 		ROOT_URL = 'https://en.wikipedia.org/wiki/List_of_terrorist_incidents'
@@ -112,8 +150,13 @@ def main():
 	page_links = get_page_links(driver, ROOT_URL, args.years, list_links)
 	write_page_links(page_links, links_outfile)
 	page_links = read_page_links(links_outfile)
-	incidents = get_all_incidents(driver, page_links, args.outfile)
+	if args.data_keys is None:
+		data_keys = ['Injured', 'Dead', 'Location', 'Date', 'Perpetrators', 'Type', 'Details']
+	else:
+		data_keys = args.data_keys
+	incidents = get_all_incidents(driver, page_links, args.outfile, data_keys)
 	write_all_incidents(incidents, args.outfile)
+	write_incidents_to_csv(incidents, csv_outfile, data_keys)
 	driver.close()
 
 if __name__ == "__main__" :
